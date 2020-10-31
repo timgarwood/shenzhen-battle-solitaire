11e286390ea3739b7d4173545ffa8deeb437001b
unknown <timgarwood@gmail.com>
2020-10-31 10:16:12 -0400

    fix chat component

--- client/src/components/InGame/Chat/ChatComponent.js
+++ client/src/components/InGame/Chat/ChatComponent.js
@@ -11,6 +11,7 @@ export default class ChatComponent extends Component {
     constructor() {
         super();
         this.chatWindowRef = React.createRef();
+        this.chatBoxRef = React.createRef();
     }
 
     componentWillMount() {
@@ -20,14 +21,19 @@ export default class ChatComponent extends Component {
     }
 
     submitMessage = () => {
-        var message = {
-            gameName: this.props.gameName,
-            sender: this.props.username,
-            messageBody: this.chatMessage,
-            timestamp: new Date(Date.now()).toUTCString()
-        }
+        if (this.chatMessage && this.chatMessage != "") {
+            var message = {
+                gameName: this.props.gameName,
+                sender: this.props.username,
+                messageBody: this.chatMessage,
+                timestamp: new Date(Date.now()).toUTCString()
+            }
 
-        this.props.socket.emit(this.props.chatMessageTopic, message)
+            this.props.socket.emit(this.props.chatMessageTopic, message)
+
+            this.chatMessage = "";
+            this.chatBoxRef.current.value = "";
+        }
     }
 
     handleChatMessage(chatMessage) {
@@ -47,20 +53,36 @@ export default class ChatComponent extends Component {
 
     render() {
         let messages = this.state.chatMessages.map(cm => {
+            let senderClass = "chat-message-mine";
+            if (cm.sender !== this.props.username) {
+                senderClass = "chat-message-yours";
+            }
             return (
-                <li className="chat-message">{cm.timestamp} {cm.sender}: {cm.messageBody}</li>
+                <div>
+                    <li className={`chat-message-timestamp`}>
+                        {cm.timestamp}
+                    </li>
+                    <li className={`chat-message ${senderClass}`}>
+                        <b>{cm.sender}</b>:{cm.messageBody}
+                    </li>
+                </div>
             );
         });
 
         return (
-            <div>
+            <div className="ChatComponent">
                 <div ref={this.chatWindowRef} className="chat-window">
-                    <ul className="chat-list">
+                    <ul>
                         {messages}
                     </ul>
                 </div>
-                <input className="chat-textbox" type="text" onChange={this.chatMessageChanged}></input>
-                <button className="chat-button" onClick={() => this.submitMessage()}>Chat</button>
+
+                <textarea className="chat-textbox"
+                    ref={this.chatBoxRef}
+                    onChange={this.chatMessageChanged}></textarea>
+
+                <button className="chat-button"
+                    onClick={() => this.submitMessage()}>Chat</button>
             </div>
         );
     }
